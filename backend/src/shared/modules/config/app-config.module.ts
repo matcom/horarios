40b874afe1d47cdb53b645e96abcd854611d0138
JCH97 <jc1997hdez@gmail.com>
2022-04-10 15:25:13 -0400

    feat(core): fix details in base layer

--- backend/src/shared/modules/config/app-config.module.ts
+++ backend/src/shared/modules/config/app-config.module.ts
@@ -1,17 +1,8 @@
-import { Module, Global } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import { AppConfigService } from './service/app-config-service';
-import { appConfig, appSchema } from './namespaces/app.config';
-import { databaseSchema, databaseConfig } from './namespaces/database.config';
-import { emailSchema, emailConfig } from './namespaces/email.config';
-import { graphqlConfig, graphqlSchema } from './namespaces/graphql.config';
-import { erpSchema, erpConfig } from './namespaces/erp.config';
-import { syncSchema, syncConfig } from './namespaces/sync.config';
-import {
-  inventoryClientConfig,
-  inventoryClientSchema,
-} from './namespaces/inventoryClient.config';
+import { databaseConfig, databaseSchema } from './namespaces/database.config';
 
 @Global()
 @Module({
@@ -19,22 +10,22 @@ import {
     ConfigModule.forRoot({
       isGlobal: true,
       load: [
-        syncConfig,
-        appConfig,
-        erpConfig,
+        // syncConfig,
+        // appConfig,
+        // erpConfig,
         databaseConfig,
-        emailConfig,
-        graphqlConfig,
-        inventoryClientConfig,
+        // emailConfig,
+        // graphqlConfig,
+        // inventoryClientConfig,
       ],
       validationSchema: Joi.object({
-        ...appSchema,
+        // ...appSchema,
         ...databaseSchema,
-        ...emailSchema,
-        ...graphqlSchema,
-        ...erpSchema,
-        ...syncSchema,
-        ...inventoryClientSchema,
+        // ...emailSchema,
+        // ...graphqlSchema,
+        // ...erpSchema,
+        // ...syncSchema,
+        // ...inventoryClientSchema,
       }),
       validationOptions: { abortEarly: true },
     }),
