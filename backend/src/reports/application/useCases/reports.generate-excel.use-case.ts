373c09e18e563b36ee7dafb135ca3865249f4e7b
JCH97 <jc997hdez@gmail.com>
2022-08-24 11:41:15 -0400

    feat(class)[develop]: backend - fix details in report.

--- backend/src/reports/application/useCases/reports.generate-excel.use-case.ts
+++ backend/src/reports/application/useCases/reports.generate-excel.use-case.ts
@@ -8,6 +8,7 @@ import { FindAllLocalUseCase } from '../../../local/application/useCases';
 import { ClassMappers } from '../../../class/infra/mappers/class.mapper';
 import * as moment from 'moment';
 import * as _ from 'underscore';
+import { FindAllLessonUseCase } from '../../../lesson/application/useCases';
 
 @Injectable()
 export class GenerateExcelReport {
@@ -16,7 +17,8 @@ export class GenerateExcelReport {
   constructor(
     private readonly findClassWithDetailsUseCase: FindAllWithDetailsClassUseCase,
     private readonly findGroupsUseCase: FindAllGroupUseCase,
-    private readonly findLocals: FindAllLocalUseCase) {
+    private readonly findLocals: FindAllLocalUseCase,
+    private readonly findLessons: FindAllLessonUseCase) {
     this._logger = new Logger('GenerateExcelReport');
   }
 
@@ -36,8 +38,8 @@ export class GenerateExcelReport {
     let nameStartRow = 4; // begin for name of days (row)
     let nameStartColumn = 2; // begin for name of days (column)
 
-
     for (let i = 0; i < groups.length; ++i) {
+
       const g = groups[i];
       const classes = (await this.findClassWithDetailsUseCase.execute({ filter: { groupId: g._id.toString() } }))
         .value
@@ -47,14 +49,18 @@ export class GenerateExcelReport {
 
       if (classes.length == 0) continue;
 
-      const grouped = _.groupBy(classes, (c) => c.week.id);
+      const grouped = _.groupBy(classes, (c) => c.week.id); // grouping by week
 
       let ws = doc.addWorksheet(g.fullName);
+      let year = 0;
 
       for (let i = nameStartColumn; i < nameStartColumn + 5; ++i) { // name of days
         ws.cell(nameStartRow, i)
           .string(moment().day(i - nameStartColumn + 1).format('dddd'))
           .style({
+            font: {
+              italics: true,
+            },
             alignment: {
               horizontal: 'center',
               vertical: 'center',
@@ -64,7 +70,11 @@ export class GenerateExcelReport {
 
       let startRow = nameStartRow + 1;
       let startColumn = nameStartColumn;
-      for (let key in grouped) {  // walking for each week
+
+      // walking for each week
+      for (let key in grouped) {
+
+        let maxLessonInADay = 10;
 
         const classes = grouped[key]
           .sort((a, b) => a.start.getTime() - b.start.getTime());
@@ -73,14 +83,14 @@ export class GenerateExcelReport {
 
         let start = moment(classes[0].week.firstDate);
         let end = moment(classes[0].week.endDate);
+        year = classes[0].lesson.year;
 
         let row = startRow;
         let column = startColumn;
 
         const weekLabel = `Semana ${start.format('DD/MM')}  - ${end.format('DD/MM')}`;
-
         ws
-          .cell(nameStartRow - 1, nameStartColumn + 2)
+          .cell(startRow - 2, startColumn + 2)
           .string(weekLabel)
           .style({
             font: {
@@ -90,9 +100,12 @@ export class GenerateExcelReport {
             alignment: { horizontal: 'center' },
           });
 
-        while (start.isBefore(end)) { // explore a specific week
+        // explore specific week
+        while (start.isBefore(end)) {
           const now = classes
             .filter(c => moment(c.start).isSame(start, 'day')); // day's classes
+
+          maxLessonInADay = Math.max(maxLessonInADay, now.length + 5);
 
           now.forEach(c => {
             ws.cell(row, column)
@@ -110,9 +123,75 @@ export class GenerateExcelReport {
           column++;
         }
 
-        startRow += 10;
+        startRow += maxLessonInADay;
       }
+
+      await this.makeLegend(ws, year);
+
     }
     doc.write('report.xlsx');
+  }
+
+  private async makeLegend(ws: xl.Worksheet, year: number) {
+    let row = 3;
+    let column = 9;
+
+    ws
+      .cell(row, column + 1)
+      .string('Asignaturas')
+      .style({
+        font: {
+          bold: true,
+          size: 12,
+        },
+        alignment: { horizontal: 'center' },
+      });
+
+    const lessons = (await this.findLessons.execute({ filter: { year: year } }))
+      .value
+      .unwrap()
+      .items;
+
+    row += 1;
+    for (let i = 0; i < lessons.length; ++i, row += i) {
+      ws
+        .cell(row, column)
+        .string(lessons[i].shortName)
+        .style({
+          alignment: { horizontal: 'center' },
+        });
+
+      ws
+        .cell(row, column + 1)
+        .string(lessons[i].fullName);
+    }
+
+
+    row += 2;
+
+    ws
+      .cell(row, column + 1)
+      .string('Horario de los turnos')
+      .style({
+        font: {
+          bold: true,
+          size: 12,
+        },
+        alignment: { horizontal: 'center' },
+      });
+
+    let start = moment('08:00', 'HH:mm');
+    for (let i = 0; i < 6; ++i) {
+      ws
+        .cell(row + i + 1, column)
+        .string(`${i + 1}º turno`)
+        .style({
+          alignment: { horizontal: 'center' },
+        });
+
+      ws
+        .cell(row + i + 1, column + 1)
+        .string(`${start.format('HH:mm')} - ${start.add(1.5, 'hours').format('HH:mm')}`);
+    }
   }
 }
--- backend/src/reports/report.module.ts
+++ backend/src/reports/report.module.ts
@@ -4,12 +4,14 @@ import { ClassModule } from '../class/class.module';
 import { GroupModule } from '../group/group.module';
 import { LocalModule } from '../local/local.module';
 import { ReportsController } from './presentation/controllers/reports.controller';
+import { LessonModule } from '../lesson/lesson.module';
 
 @Module({
   imports: [
     ClassModule,
     GroupModule,
     LocalModule,
+    LessonModule
   ],
   providers: [GenerateExcelReport],
   exports: [GenerateExcelReport],
